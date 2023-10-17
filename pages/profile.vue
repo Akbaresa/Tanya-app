f7060af7fa474d3daa66bf650986daf2091dd9d2
nda00megship <anandamp100@gmail.com>
2023-10-17 15:36:23 +0700

    profile patch 1.0

---
+++ pages/profile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Profile></Profile>
+    </div>
+</template>
+
+<script>
+import Profile from './profile/Profile.vue'
+export default {
+    components:{
+        Profile
+    },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
