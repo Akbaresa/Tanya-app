d35e8cb8dc548248e63a134907f6dbfa6fb305d7
esa <takutoakbar354@gmail.com>
2023-10-30 15:12:53 +0700

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     "./pages/app.vue",
     "./node_modules/flowbite/**/*.{js,ts}",
     'node_modules/preline/dist/*.js',
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
@@ -15,7 +16,10 @@ export default {
   plugins: [
     require('flowbite/plugin'),
     require('preline/plugin'),
+    require("tw-elements/dist/plugin.cjs"),
+    
 
   ],
+  darkMode: "class"
 }
 
